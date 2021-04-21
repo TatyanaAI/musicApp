372ec051401c4795fd4d776b4efb31d9bee31176
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-04-21 15:38:11 +0600

    #1 TICKET: Добавлены основные endpointы и подключена БД

---
+++ lesson_82_back/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const artists = require("./app/artists");
+const albums = require("./app/albums");
+const tracks = require("./app/tracks");
+const app = express();
+
+const port = 8000;
+
+const run = async () => {
+  await mongoose.connect("mongodb://localhost/musicApp", {useNewUrlParser: true, useUnifiedTopology: true});
+
+  app.use(cors());
+  app.use(express.static('public'));
+  app.use(express.json());
+  app.use("/artists", artists());
+  app.use("/albums", albums());
+  app.use("/tracks", tracks());
+
+  app.listen(port, () => {
+    console.log("Server started at http://localhost:" + port);
+  });
+};
+
+run().catch(e => console.log(e));
