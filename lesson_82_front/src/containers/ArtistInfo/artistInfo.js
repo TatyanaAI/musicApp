9458ce5cb4db0817f9ce958d3c7498e387806ca7
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-05-05 15:26:38 +0600

    #8 TICKET (Lesson_85):  Добавлен компонент-контейнер ArtistInfo, в котором на основе хуков useSelector и useDispatch и ранее созданной функции с запросом к БД получены и отражены соответствующие данные по альбомам исполнителя, как и в artists каждый раз при загрузке предварительно производится зачистка для возврата к изначальному состоянию. Фактическое время работы: 3 часа 15 минут.

---
+++ lesson_82_front/src/containers/ArtistInfo/artistInfo.js
@@ -0,0 +1,56 @@
+import { Grid } from "@material-ui/core";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { artistRequest, setArtist } from "../../store/actions/artistsActions";
+import { albumsRequest, initAlbum } from "../../store/actions/albumsActions";
+import AlbumInfoItem from "../../components/AlbumInfoItem/albumInfoItem"
+import BackDrop from '../../components/UI/BackDrop/backDrop'
+
+const ArtistInfo = (props) => {
+    const id = props.match.params.id;
+
+    const dispatch = useDispatch();
+    const { artists } = useSelector(state => state.artists);
+    const { albums, loading, error } = useSelector(state => state.albums);
+
+    useEffect(() => {
+        dispatch(initAlbum());
+
+        const artist = artists.find(artist => artist.id === id);
+        if (artist) {
+            dispatch(setArtist(artist));
+        } else {
+            dispatch(artistRequest(id));
+        }
+
+        dispatch(albumsRequest(id));
+    }, [dispatch, id]);
+
+    return (
+        <>
+            <BackDrop loading={loading} />
+            <Grid container
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
+                spacing={10}
+            >
+                {albums.map(album => {
+                    return <AlbumInfoItem
+                        id={album.id}
+                        title={album.title}
+                        year={album.year}
+                        cover={album.cover}
+                        key={album.id}
+                        error={error}
+                    />
+                })}
+            </Grid>
+
+
+        </>
+
+    );
+};
+
+export default ArtistInfo;
