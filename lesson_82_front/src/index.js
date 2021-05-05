fd2d2583c62c0c1ccfd34467f09a07dfc6862fbe
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-05-05 14:34:28 +0600

    #7 TICKET (Lesson_85): Создана структура frontend части музыкального приложения, добавлены необходимые зависимости.

---
+++ lesson_82_front/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware, ConnectedRouter } from "connected-react-router";
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import artistsReducer from './store/reducers/artistsReducer';
+import albumsReducer from "./store/reducers/albumsReducer";
+import tracksReducer from "./store/reducers/tracksReducer";
+
+axios.defaults.baseURL = 'http://localhost:8000';
+const history = createBrowserHistory();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  artists: artistsReducer,
+  albums: albumsReducer,
+  tracks: tracksReducer,
+  router: connectRouter(history)
+});
+
+const middleware = [
+  thunkMiddleware,
+  routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, enhancers);
+
+
+const app = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
