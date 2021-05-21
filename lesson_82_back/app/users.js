35d3edaf68ffe2aefa79e81e78bd5802afddb43e
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-05-21 12:36:13 +0600

    #13 TICKET: Добавлен в функционал залогиненного пользователя возможность выхода из сессии/логаута с помощью DELETE-запроса и генерации нового токена.

--- lesson_82_back/app/users.js
+++ lesson_82_back/app/users.js
@@ -2,15 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-
 const createRouter = () => {
     router.post("/", async (req, res) => {
+        if (!req.body.username)
+            return res.status(400).send({ error: 'Username parameter not found' });
+        if (!req.body.password)
+            return res.status(400).send({ error: 'Password parameter not found' });
+
         let result = await User.findOne({ username: req.body.username })
         if (result) {
-            return res.send(result);
+            return res.status(400).send({ error: 'Username already exists' });
         }
-        const user = { ...req.body };
-        result = new User(user);
+
+        result = new User({
+            username: req.body.username,
+            password: req.body.password,
+          });
+
         result.generateToken();
         try {
             await result.save();
@@ -45,8 +53,26 @@ const createRouter = () => {
         }
     });
 
+    router.delete("/sessions", async (req, res) => {
+        const token = req.get("Authentication");
+        const success = { message: "Success" };
+
+        if (!token) return res.send(success);
+
+        const user = await User.findOne({ token });
+
+        if (!user) return res.send(success);
+
+        user.generateToken();
+        try {
+            await user.save({ validateBeforeSave: false });
+            return res.send(success);
+        } catch (e) {
+            res.status(500).send(e);
+        }
+    });
+
     return router;
 };
-
 
 module.exports = createRouter;
